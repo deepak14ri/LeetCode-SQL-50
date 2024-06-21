b1d1f123fba7987a90e452c8124cd27d6bc19953
deepak14ri <deepakchaudhari705@gmail.com>
2024-06-21 23:48:50 +0530

    consecutive numbser

---
+++ ADVANCED SELECT AND JOINS/180. Consecutive Numbers.sql
@@ -0,0 +1,5 @@
+-- Write your MySQL/PostgreSQL query statement below
+SELECT DISTINCT l1.num as ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2 ON l1.num = l2.num and l1.id = l2.id + 1
+JOIN Logs l3 ON l2.num = l3.num and l2.id = l3.id + 1;
